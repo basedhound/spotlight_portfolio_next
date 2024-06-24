c8feb5a7809f525c291c80bbe51341b22f05d21a
Jozef S <franck.vukelic@gmail.com>
2024-06-24 16:06:05 +0700

    Add Dark theme using next-theme library

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-// import { ThemeProvider } from "./provider";
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,14 +22,14 @@ export default function RootLayout({
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
-        {/* <ThemeProvider
+         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
-        > */}
+        > 
           {children}
-        {/* </ThemeProvider> */}
+         </ThemeProvider>
       </body>
     </html>
   );
