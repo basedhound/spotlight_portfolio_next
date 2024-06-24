b1c875ab3d20937b3664644201428d9fb8785b27
Jozef S <franck.vukelic@gmail.com>
2024-06-24 16:05:41 +0700

    Add Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div>
+      <div className="max-w-7xl w-full">
+        <Hero />
         <h1>Hello</h1>
       </div>
     </main>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
        *  0.2 to 0.03
        */}
       <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         {/* Radial gradient for the container to give a faded look */}
@@ -58,7 +58,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Frank, a Next.js Developer based in France.
           </p>
 
           <a href="#about">
