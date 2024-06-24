7b95f78fab8e855470dda6244c42333becd14fe3
Jozef S <franck.vukelic@gmail.com>
2024-06-24 16:31:13 +0700

    Add NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Hero from "@/components/Hero";
+import { navItems } from "@/data";
 
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+//!
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+      <FloatingNav navItems={navItems} />
         <Hero />
         <h1>Hello</h1>
       </div>
