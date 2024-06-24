39034bc96f904af6b6c8d8a7f0b2910af53d8fe9
Jozef S <franck.vukelic@gmail.com>
2024-06-24 18:25:47 +0700

    Fix Footer

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,18 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 //!
 export default function Home() {
   return (
+    
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-      <FloatingNav navItems={navItems} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
         <Approach />
-        <Footer />
       </div>
+      <Footer />
     </main>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light mb-3">
+        <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 
         </p>
 
