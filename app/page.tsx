55801cb95a2bd7af173ba182399c2ff13f802976
Jozef S <franck.vukelic@gmail.com>
2024-06-24 17:54:18 +0700

    Add Clients section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 //
 import { navItems } from "@/data";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 //!
 export default function Home() {
@@ -15,6 +19,10 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
+        {/* <Experience /> */}
+        {/* <Approach /> */}
+        {/* <Footer /> */}
       </div>
     </main>
   );
