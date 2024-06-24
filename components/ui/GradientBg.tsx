7d055454d11bdc4d16fb6f4f6b5bd5eb4a4cfb26
Jozef S <franck.vukelic@gmail.com>
2024-06-24 17:39:54 +0700

    Format

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -91,8 +91,7 @@ export const BackgroundGradientAnimation = ({
       className={cn(
         "w-full h-full absolute overflow-hidden top-0 left-0 bg-[linear-gradient(40deg,var(--gradient-background-start),var(--gradient-background-end))]",
         containerClassName
-      )}
-    >
+      )}>
       <svg className="hidden">
         <defs>
           <filter id="blurMe">
@@ -116,8 +115,7 @@ export const BackgroundGradientAnimation = ({
         className={cn(
           "gradients-container h-full w-full blur-lg",
           isSafari ? "blur-2xl" : "[filter:url(#blurMe)_blur(40px)]"
-        )}
-      >
+        )}>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_var(--first-color)_0,_var(--first-color)_50%)_no-repeat]`,
@@ -125,8 +123,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:center_center]`,
             `animate-first`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--second-color),_0.8)_0,_rgba(var(--second-color),_0)_50%)_no-repeat]`,
@@ -134,8 +131,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%-400px)]`,
             `animate-second`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--third-color),_0.8)_0,_rgba(var(--third-color),_0)_50%)_no-repeat]`,
@@ -143,8 +139,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%+400px)]`,
             `animate-third`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fourth-color),_0.8)_0,_rgba(var(--fourth-color),_0)_50%)_no-repeat]`,
@@ -152,8 +147,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%-200px)]`,
             `animate-fourth`,
             `opacity-70`
-          )}
-        ></div>
+          )}></div>
         <div
           className={cn(
             `absolute [background:radial-gradient(circle_at_center,_rgba(var(--fifth-color),_0.8)_0,_rgba(var(--fifth-color),_0)_50%)_no-repeat]`,
@@ -161,8 +155,7 @@ export const BackgroundGradientAnimation = ({
             `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,
             `animate-fifth`,
             `opacity-100`
-          )}
-        ></div>
+          )}></div>
 
         {interactive && (
           <div
@@ -172,8 +165,7 @@ export const BackgroundGradientAnimation = ({
               `absolute [background:radial-gradient(circle_at_center,_rgba(var(--pointer-color),_0.8)_0,_rgba(var(--pointer-color),_0)_50%)_no-repeat]`,
               `[mix-blend-mode:var(--blending-value)] w-full h-full -top-1/2 -left-1/2`,
               `opacity-70`
-            )}
-          ></div>
+            )}></div>
         )}
       </div>
     </div>
