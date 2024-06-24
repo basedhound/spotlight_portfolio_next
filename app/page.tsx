47c53c6d1f82df781aedb6529ffe706e6a2591fd
Jozef S <franck.vukelic@gmail.com>
2024-06-24 17:54:32 +0700

    Add Experience section

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
-        {/* <Experience /> */}
+        <Experience />
         {/* <Approach /> */}
         {/* <Footer /> */}
       </div>
